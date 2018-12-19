75f3976365234cca0ef16f45804c5d4e8bad2bf1
Vishva deepak Tripathi <vishvadeepaktripathi@outlook.com>
2018-12-19 18:37:49 +0530

    Call module file
    this is simple file wihich will use to call classes and run the classes for output

---
+++ src/CallModule.py
@@ -0,0 +1,9 @@
+import TableClass
+mytableclass=TableClass.TableClass()
+#mytableclass.even_odd(5)
+#mytableclass.even_odd(6)
+#mytableclass.userTable()
+mytableclass.nameInput()
+#mytableclass.countTable()
+#w = mytableclass.even_odd(31)
+#w is None
