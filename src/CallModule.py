62e6ef012c83e246578426bf1b6ba8a80592ffbc
Vishva deepak Tripathi <vishvadeepaktripathi@outlook.com>
2018-12-19 19:04:22 +0530

    Change into run function for output

--- src/CallModule.py
+++ src/CallModule.py
@@ -1,8 +1,8 @@
 import TableClass
 mytableclass=TableClass.TableClass()
-#mytableclass.even_odd(5)
-#mytableclass.even_odd(6)
-#mytableclass.userTable()
+mytableclass.even_odd(5)
+mytableclass.even_odd(6)
+mytableclass.userTable()
 mytableclass.nameInput()
 #mytableclass.countTable()
 #w = mytableclass.even_odd(31)
