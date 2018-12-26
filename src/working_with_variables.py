df14aa9c5539be8b4bdb1143dc5dd3973e4cc78c
Vishva deepak Tripathi <vishvadeepaktripathi@outlook.com>
2018-12-26 16:24:07 +0530

    Create working_with_variables.py
    define variable and print the value store in variable.
    Also you can chec the type of the variable and print into different ways.
    using str()

---
+++ src/working_with_variables.py
@@ -0,0 +1,26 @@
+'''
+Created on Dec 26, 2018
+
+@author: Vishva 
+'''
+
+# Define Variable and find the type of the variable 
+Name = 'Jhon ' #string variable 
+Age =  23 #int variable 
+Salary =10000.00 #float variable 
+Address = 'Sector 21, Haryana, Gurugram' #String Variable 
+
+#### Print the name iin simple formate with print function 
+print(Name);
+print(Age);
+print(Salary);
+print(Address);
+#### End of the print function with the print()
+print('###################################################################3')
+# check the type and print the type on the console 
+## use str() this will convert the object into string that will help to print the function 
+print('Name is ' + str(type(Name)) + ' type');
+print('Age is ' + str(type(Age)) +' type ') ;
+print('Salary is '+ str(type(Salary)) +' type ');
+print('Address is '+ str(type(Address))+' type ');
+
