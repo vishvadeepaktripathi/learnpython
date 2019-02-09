18eab2415e999b6bf2e00fab19ee7d49df8c2272
Vishva deepak Tripathi <vishvadeepaktripathi@outlook.com>
2019-02-09 21:18:52 +0530

    Update CallModule.py
    Addition module with type conversation
    with input variable

--- src/CallModule.py
+++ src/CallModule.py
@@ -1,9 +1,10 @@
 import TableClass
 mytableclass=TableClass.TableClass()
-mytableclass.even_odd(5)
-mytableclass.even_odd(6)
-mytableclass.userTable()
-mytableclass.nameInput()
+#mytableclass.even_odd(5)
+#mytableclass.even_odd(6)
+#mytableclass.userTable()
+#mytableclass.nameInput()
+mytableclass.addition()
 #mytableclass.countTable()
 #w = mytableclass.even_odd(31)
 #w is None
