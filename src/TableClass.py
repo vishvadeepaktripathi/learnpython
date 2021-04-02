dd404dbd0ffdb38e3c8425e29fe45e46a53d7b07
vishvadeepak.tripathi <vishvadeepak.tripathi@siemens.com>
2021-04-02 16:34:32 +0530

    call module change and table class

--- src/TableClass.py
+++ src/TableClass.py
@@ -4,6 +4,10 @@ Created on Dec 19, 2018
 @author: Vishva Deepak Tripathi
 '''
 from _ast import Num
+import time
+import datetime
+from datetime import date
+from _datetime import datetime_CAPI
 
 class TableClass():
     def userTable(self):
@@ -50,6 +54,21 @@ class TableClass():
     def squarecalcualtion(self):
         Num1 = float(input('Enter the number that you want to square : '))
         print(Num1**2)
+    
+    @staticmethod
+    def CompareDate(self):
+        date_1 = date.today()
+        end_date = date_1 + datetime.timedelta(days=10)
+        end_date.strftime("%x")
+        print(self)
+        dt_new=time.strptime(self, "%x")
+        print(str(dt_new))
+        #datetime.strptime(dt,"%x").date()
+        dt_new=time.strptime(self,"%x")
+        #dt_new=datetime.date(2020,7,19)
+        print(end_date)
+        if dt_new!=end_date:
+            print("Date is not equal")
         
     #def __init__(self):
         #self.x=x
